fe775bc23608a645c83fe0b649e7922c14d9d5f7
Pedro <Pedrocandidopanzer2005@hotmail.com>
2023-11-13 12:03:13 -0300

    atualização

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -27,24 +27,24 @@
 		hour = minute * 60,
 		day = hour * 24;
 
-	// let countDown = new Date('December 0, 2023 17:30:00').getTime(),
-	// 	x = setInterval(function () {
+	let countDown = new Date('December 9, 2023 19:30:00').getTime(),
+		x = setInterval(function () {
 
-	// 		let now = new Date().getTime(),
-	// 			distance = countDown - now;
+			let now = new Date().getTime(),
+				distance = countDown - now;
 
-	// 		document.getElementById('days').innerText = Math.floor(distance / (day)),
-	// 			document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
-	// 			document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
-	// 			document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+			document.getElementById('days').innerText = Math.floor(distance / (day)),
+				document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+				document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+				document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
 
-	// 		//do something later when date is reached
-	// 		//if (distance < 0) {
-	// 		//  clearInterval(x);
-	// 		//  'IT'S MY BIRTHDAY!;
-	// 		//}
+			// do something later when date is reached
+			// if (distance < 0) {
+			//  clearInterval(x);
+			//  'IT'S MY BIRTHDAY!;
+			// }
 
-	// 	}, second)
+		}, second)
 
 	$(function () {
 		$("#tabs").tabs();
